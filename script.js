88b9e5833db71c2e7f08becf10c9880770a33834
Christopher Le <Chrisle808@yahoo.com>
2022-01-13 21:54:35 -0800

    updated HTML CSS and JS

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-var requestUrl = 'https://api.jikan.moe/v3/anime/1735';
-function getApi(request) {
-  fetch(request)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+var searchBtn = document.querySelector('button');
+var searchVal = document.querySelector('.searchVal');
+var requestUrl;
 
+
+function getUrl() {
+ requestUrl = 'https://api.jikan.moe/v3/search/anime?q=Naruto' +  searchVal.value; 
+ console.log(requestUrl);
+ function getApi(request) {
+    fetch(request)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  
+  }
+ getApi(requestUrl);
 }
-
-getApi(requestUrl);
+searchBtn.addEventListener('click', getUrl());
