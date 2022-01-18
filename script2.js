9c9c11902d0f1f8eb6168f7764daef4d64f11638
Christopher Le <Chrisle808@yahoo.com>
2022-01-18 00:43:30 -0800

    made final changes to all files

--- script2.js
+++ script2.js
@@ -1,7 +1,13 @@
-var searchBtn = document.querySelector('button');
 var searchVal = document.querySelector('.searchVal');
 var cardBody = document.getElementById('card');
 var displayT = document.getElementById('time');
+var btn;
+var addBtn = document.getElementById('addBtn');
+var addText = document.getElementById('addText');
+var order = document.querySelector('ol');
+var obj = [];
+var obj2 = [];
+var backBtn = document.querySelector('.back');
 
 document.getElementById('myButton').addEventListener('click', function(){
     console.log(searchVal.value);
@@ -36,10 +42,16 @@ for(let i = 0; i < 16; i++) {
     cardTitle.setAttribute('class', 'card_name');
 
     const title = document.createElement('span');
+    title.setAttribute('id', 'listName');
     title.innerHTML = data.results[i].title;
+
+    btn = document.createElement('button');
+    btn.setAttribute('class', 'Add');
+    btn.innerHTML = 'Add to List';
 
     listTag.setAttribute('class', 'cell');
 
+    listTag.appendChild(btn);
     aTag.append(cardImage);
     cardImage.append(image);
     aTag.append(cardTitle);
@@ -49,6 +61,46 @@ for(let i = 0; i < 16; i++) {
 }
 })
 }
+
+addBtn.addEventListener('click', addToList);
+
+function addToList() {
+    obj.push(addText.value);
+    addText.value = '';
+    const myJSON = JSON.stringify(obj);
+    localStorage.setItem('title', myJSON);
+    getList();
+}
+
+function getList() {
+    order.innerHTML = '';
+    obj = JSON.parse(localStorage.getItem('title'));
+    for(let i = 0; i < obj.length; i++) {
+        var item = document.createElement('li');
+        item.innerHTML = obj[i];
+        order.append(item);
+    }
+    
+}
+
+refresh();
+
+function refresh() {
+    obj2 = JSON.parse(localStorage.getItem('title'));
+    for(let i = 0; i < obj2.length; i++) {
+        var item = document.createElement('li');
+        item.innerHTML = obj2[i];
+        order.append(item);
+    }
+}
+
+backBtn.addEventListener('click', home);
+
+function home() {
+    window.location.assign('file:///C:/Users/chris/code/Project1/Project-AnimeSE/index.html');
+}
+
+
 
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY hh:mm:ss a');
